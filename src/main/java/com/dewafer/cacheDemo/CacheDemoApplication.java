f208a2a1ff191b01207443980261c2ec16711d97
dewafer <dewafer@gmail.com>
2018-09-08 12:53:19 +0800

    Fist commit

---
+++ src/main/java/com/dewafer/cacheDemo/CacheDemoApplication.java
@@ -0,0 +1,12 @@
+package com.dewafer.cacheDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheDemoApplication.class, args);
+	}
+}
